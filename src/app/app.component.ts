68c7f4cf7229c97186c8d7ecfb84f91692e377bd
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2018-05-14 16:15:46 +0400

    Lodash installed and way to load only needed methods used in app.component.ts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 
+//lodash
+import {
+  each,
+  pickBy,
+  find
+} from 'lodash';
+
+//Technique to avoid loading whole lodash library into components => smaller module bundle size
+const _ = {
+  each,
+  pickBy,
+  find
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
