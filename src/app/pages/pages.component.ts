731caa1b7c95057812e7855af9e37812a4da6ebe
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2018-05-16 16:42:52 +0400

    Code refactor: Spacing

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-
 import { MENU_ITEMS } from './pages-menu';
 
 @Component({
   selector: 'pages',
   templateUrl: `./pages.component.html`
 })
+
 export class PagesComponent {
   menu = MENU_ITEMS;
 }
