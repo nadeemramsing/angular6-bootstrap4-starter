4e5785bf6adcb89570d358785f2700316893ebbf
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2018-05-18 14:47:14 +0400

    BUG FIX: Was using existing HTML element (form) as selector) => it got default css styling given by browser.

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'form',
+  selector: 'forms',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
