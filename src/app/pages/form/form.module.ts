497c1c88d1015eea916e5b12386f3a7c711ad5d2
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2018-05-21 15:19:39 +0400

    Error messages / validation working.

--- src/app/pages/form/form.module.ts
+++ src/app/pages/form/form.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+
+import { CustomFormsModule } from 'ng4-validators';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { FormRoutingModule, routedComponents } from './form-routing.module';
 
 @NgModule({
   imports: [
-    FormRoutingModule
+    CommonModule,
+    CustomFormsModule,
+    FormRoutingModule,
+    FormsModule,
+    MDBBootstrapModule,
+    ReactiveFormsModule
   ],
   declarations: [
     ...routedComponents
--- src/app/pages/form/form1/form1.component.ts
+++ src/app/pages/form/form1/form1.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators, ValidationErrors, FormBuilder } from '@angular/forms';
+import { of } from 'rxjs';
+import { size } from 'lodash';
 
 @Component({
   selector: 'form1',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form1.component.css']
 })
 export class Form1Component implements OnInit {
+  commentFormGroup: FormGroup;
 
-  constructor() { }
+  nameFormControl: AbstractControl;
+  emailFormControl: AbstractControl;
+  bodyFormControl: AbstractControl;
+
+  _: Object = { size };
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    //FormGroup init
+    //[controlsConfig, ...extra: ValidatorFn || AsyncValidatorFn] <- ~FormGroup
+    this.commentFormGroup = this.fb.group({
+      'name': ['Ramsing', Validators.compose([Validators.minLength(10), customValidator])],
+      'email': ['nadeem.ramsing@chb-technologies.ch'],
+      'body': [],
+    });
+
+    //FormControl init
+    this.nameFormControl = this.commentFormGroup.controls['name'];
+    this.emailFormControl = this.commentFormGroup.controls['email'];
+    this.bodyFormControl = this.commentFormGroup.controls['body'];
   }
+
+}
+
+/* HELPER FUNCTIONS */
+function customValidator(control: FormControl): ValidationErrors {
+  let name = control.value || '';
+
+  if (name.toLocaleLowerCase() === 'nadeem')
+    return {
+      custom: 'Nadeem'
+    }
+
+  return null;
 
 }
