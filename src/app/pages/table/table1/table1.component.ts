c5ae9e0e5f38982cc44f6e3a5e44b2a45d28abf4
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2018-05-23 16:05:06 +0400

    router-outlet was missing in table.component.ts for table1 component to load.

--- src/app/pages/table/table1/table1.component.ts
+++ src/app/pages/table/table1/table1.component.ts
@@ -3,14 +3,16 @@ import { Component, OnInit } from '@angular/core';
 import { CommentService } from './../../../common/services/comment.service';
 
 @Component({
-  selector: 'app-table1',
+  selector: 'table1',
   templateUrl: './table1.component.html',
   styleUrls: ['./table1.component.css'],
   providers: [CommentService]
 })
 export class Table1Component implements OnInit {
 
-  constructor(/* private commentService: CommentService */) { }
+  constructor(
+    private commentService: CommentService
+  ) { }
 
   ngOnInit() {
 
